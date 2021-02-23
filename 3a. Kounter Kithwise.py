ca3d3a9053121b35b40b4d21fe7c3dd2bf0c2d6a
Zach <zach@fauser.net>
2021-02-23 12:04:12 -0500

    Add files via upload

---
+++ 3a. Kounter Kithwise.py
@@ -0,0 +1,5 @@
+# Avoid the ogres and grab the gem.
+hero.moveDown(2)
+hero.moveRight()
+hero.moveUp()
+hero.moveRight()
---
+++ 3b. Crawlways of Kithgard.py
@@ -0,0 +1,4 @@
+# Avoid being seen by the ogres.
+hero.moveRight()
+hero.moveLeft()
+hero.moveRight(2)
