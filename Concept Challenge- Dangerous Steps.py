7c0aba19321695fad3d63843192b45169246d5ab
Zach <zach@fauser.net>
2021-02-23 12:06:04 -0500

    Add files via upload

---
+++ Concept Challenge- Dangerous Steps.py
@@ -0,0 +1,20 @@
+# Defeat the ogres using their names.
+
+hero.moveRight();
+hero.attack("Sog")
+hero.attack("Sog")
+hero.attack("Gos")
+hero.attack("Gos")
+hero.say("nice")
+hero.moveDown(1.5)
+hero.moveUp(1.5)
+
+# Defeat the first pair of ogres.
+
+hero.moveRight(2);
+# Defeat the second pair of ogres.
+hero.moveUp()
+hero.attack("Ergo")
+hero.attack("Ergo")
+hero.attack("Kro")
+hero.attack("Kro")
